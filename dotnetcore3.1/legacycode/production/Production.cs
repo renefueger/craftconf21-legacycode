ea96f6da606224f0f895d612f5f704da7a39fc6e
renefueger <github@renefueger.de>
2021-05-27 11:53:24 +0200

    initial setup for dotnetcore3.1 added

---
+++ dotnetcore3.1/legacycode/production/Production.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace production
+{
+    public class Production
+    {
+    }
+}
---
+++ dotnetcore3.1/legacycode/tests/LegacyTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace tests
+{
+    [TestFixture]
+    public class LegacyTests
+    {
+        [Test]
+        public void AFailingUnitTest()
+        {
+            Assert.Fail("it is failing for the correct reasons");
+        }
+    }
+}
